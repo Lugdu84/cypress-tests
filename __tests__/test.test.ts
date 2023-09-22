929cfddb1470a7ef148faeac9df5ab17092ae316
David Grammatico <david.lugdunum@gmail.com>
2023-09-22 22:23:22 +0200

    update tests

---
+++ __tests__/test.test.ts
@@ -0,0 +1,8 @@
+// test for addition
+import { describe, it, expect } from '@jest/globals'
+
+describe('Addition', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3)
+  })
+})
--- cypress/e2e/2-advanced-examples/actions.cy.js
+++ cypress/e2e/2-advanced-examples/actions.cy.js
@@ -9,24 +9,28 @@ context('Actions', () => {
 
   it('.type() - type into a DOM element', () => {
     // https://on.cypress.io/type
-    cy.get('.action-email')
-      .type('fake@email.com').should('have.value', 'fake@email.com')
+    const mail = () => cy.get('.action-email')
+    mail()
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com')
 
       // .type() with special character sequences
       .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
       .type('{del}{selectall}{backspace}')
 
       // .type() with key modifiers
-      .type('{alt}{option}') //these are equivalent
-      .type('{ctrl}{control}') //these are equivalent
-      .type('{meta}{command}{cmd}') //these are equivalent
+      .type('{alt}{option}') // these are equivalent
+      .type('{ctrl}{control}') // these are equivalent
+      .type('{meta}{command}{cmd}') // these are equivalent
       .type('{shift}')
 
       // Delay each keypress by 0.1 sec
       .type('slow.typing@email.com', { delay: 100 })
       .should('have.value', 'slow.typing@email.com')
 
-    cy.get('.action-disabled')
+    const check = () => cy.get('.action-disabled')
+    check()
+      .get('.action-disabled')
       // Ignore error checking prior to type
       // like whether the input is visible or disabled
       .type('disabled error checking', { force: true })
@@ -35,21 +39,33 @@ context('Actions', () => {
 
   it('.focus() - focus on a DOM element', () => {
     // https://on.cypress.io/focus
-    cy.get('.action-focus').focus()
+    const focus = () => cy.get('.action-focus')
+    focus()
+      .get('.action-focus')
+      .focus()
       .should('have.class', 'focus')
-      .prev().should('have.attr', 'style', 'color: orange;')
+      .prev()
+      .should('have.attr', 'style', 'color: orange;')
   })
 
   it('.blur() - blur off a DOM element', () => {
     // https://on.cypress.io/blur
-    cy.get('.action-blur').type('About to blur').blur()
+    const blur = () => cy.get('.action-blur')
+    blur()
+      .get('.action-blur')
+      .type('About to blur')
+      .blur()
       .should('have.class', 'error')
-      .prev().should('have.attr', 'style', 'color: red;')
+      .prev()
+      .should('have.attr', 'style', 'color: red;')
   })
 
   it('.clear() - clears an input or textarea element', () => {
     // https://on.cypress.io/clear
-    cy.get('.action-clear').type('Clear this text')
+    const clear = () => cy.get('.action-clear')
+    clear()
+      .get('.action-clear')
+      .type('Clear this text')
       .should('have.value', 'Clear this text')
       .clear()
       .should('have.value', '')
@@ -57,11 +73,13 @@ context('Actions', () => {
 
   it('.submit() - submit a form', () => {
     // https://on.cypress.io/submit
-    cy.get('.action-form')
-      .find('[type="text"]').type('HALFOFF')
+    cy.get('.action-form').find('[type="text"]').type('HALFOFF')
 
     cy.get('.action-form').submit()
-      .next().should('contain', 'Your form has been submitted!')
+
+    cy.get('.action-form')
+      .next()
+      .should('contain', 'Your form has been submitted!')
   })
 
   it('.click() - click on a DOM element', () => {
@@ -96,7 +114,9 @@ context('Actions', () => {
     // .click() accepts an x and y coordinate
     // that controls where the click occurs :)
 
-    cy.get('#action-canvas')
+    const clickCanvas = () => cy.get('#action-canvas')
+    clickCanvas()
+      .get('#action-canvas')
       .click(80, 75) // click 80px on x coord and 75px on y coord
       .click(170, 75)
       .click(80, 165)
@@ -117,7 +137,9 @@ context('Actions', () => {
 
     // Our app has a listener on 'dblclick' event in our 'scripts.js'
     // that hides the div and shows an input on double click
-    cy.get('.action-div').dblclick().should('not.be.visible')
+    cy.get('.action-div').as('dblclick')
+    cy.get('@dblclick').dblclick()
+    cy.get('@dblclick').should('not.be.visible')
     cy.get('.action-input-hidden').should('be.visible')
   })
 
@@ -126,7 +148,8 @@ context('Actions', () => {
 
     // Our app has a listener on 'contextmenu' event in our 'scripts.js'
     // that hides the div and shows an input on right click
-    cy.get('.rightclick-action-div').rightclick().should('not.be.visible')
+    const rightClick = () => cy.get('.rightclick-action-div')
+    rightClick().rightclick().should('not.be.visible')
     cy.get('.rightclick-action-input-hidden').should('be.visible')
   })
 
@@ -135,26 +158,35 @@ context('Actions', () => {
 
     // By default, .check() will check all
     // matching checkbox or radio elements in succession, one after another
-    cy.get('.action-checkboxes [type="checkbox"]').not('[disabled]')
-      .check().should('be.checked')
+    const actionCheck = () => cy.get('.action-check')
+    actionCheck()
+      .get('.action-checkboxes [type="checkbox"]')
+      .not('[disabled]')
+      .check()
+      .should('be.checked')
 
-    cy.get('.action-radios [type="radio"]').not('[disabled]')
-      .check().should('be.checked')
+    cy.get('.action-radios [type="radio"]').as('radios')
+    cy.get('@radios').not('[disabled]').check()
+    cy.get('@radios').should('be.checked')
 
     // .check() accepts a value argument
-    cy.get('.action-radios [type="radio"]')
-      .check('radio1').should('be.checked')
+
+    cy.get('@radios').check('radio1')
+    cy.get('@radios').should('be.checked')
 
     // .check() accepts an array of values
     cy.get('.action-multiple-checkboxes [type="checkbox"]')
-      .check(['checkbox1', 'checkbox2']).should('be.checked')
+      .check(['checkbox1', 'checkbox2'])
+      .should('be.checked')
 
     // Ignore error checking prior to checking
     cy.get('.action-checkboxes [disabled]')
-      .check({ force: true }).should('be.checked')
+      .check({ force: true })
+      .should('be.checked')
 
     cy.get('.action-radios [type="radio"]')
-      .check('radio3', { force: true }).should('be.checked')
+      .check('radio3', { force: true })
+      .should('be.checked')
   })
 
   it('.uncheck() - uncheck a checkbox element', () => {
@@ -164,29 +196,32 @@ context('Actions', () => {
     // checkbox elements in succession, one after another
     cy.get('.action-check [type="checkbox"]')
       .not('[disabled]')
-      .uncheck().should('not.be.checked')
+      .uncheck()
+      .should('not.be.checked')
 
     // .uncheck() accepts a value argument
     cy.get('.action-check [type="checkbox"]')
       .check('checkbox1')
-      .uncheck('checkbox1').should('not.be.checked')
+      .uncheck('checkbox1')
+      .should('not.be.checked')
 
     // .uncheck() accepts an array of values
     cy.get('.action-check [type="checkbox"]')
       .check(['checkbox1', 'checkbox3'])
-      .uncheck(['checkbox1', 'checkbox3']).should('not.be.checked')
+      .uncheck(['checkbox1', 'checkbox3'])
+      .should('not.be.checked')
 
     // Ignore error checking prior to unchecking
     cy.get('.action-check [disabled]')
-      .uncheck({ force: true }).should('not.be.checked')
+      .uncheck({ force: true })
+      .should('not.be.checked')
   })
 
   it('.select() - select an option in a <select> element', () => {
     // https://on.cypress.io/select
 
     // at first, no option should be selected
-    cy.get('.action-select')
-      .should('have.value', '--Select a fruit--')
+    cy.get('.action-select').should('have.value', '--Select a fruit--')
 
     // Select option(s) with matching text content
     cy.get('.action-select').select('apples')
@@ -201,7 +236,8 @@ context('Actions', () => {
       .should('deep.equal', ['fr-apples', 'fr-oranges', 'fr-bananas'])
 
     // Select option(s) with matching value
-    cy.get('.action-select').select('fr-bananas')
+    cy.get('.action-select')
+      .select('fr-bananas')
       // can attach an assertion right away to the element
       .should('have.value', 'fr-bananas')
 
@@ -212,7 +248,8 @@ context('Actions', () => {
 
     // assert the selected values include oranges
     cy.get('.action-select-multiple')
-      .invoke('val').should('include', 'fr-oranges')
+      .invoke('val')
+      .should('include', 'fr-oranges')
   })
 
   it('.scrollIntoView() - scroll an element into view', () => {
@@ -222,26 +259,20 @@ context('Actions', () => {
     // because they're not within
     // the viewable area of their parent
     // (we need to scroll to see them)
-    cy.get('#scroll-horizontal button')
-      .should('not.be.visible')
+    cy.get('#scroll-horizontal button').should('not.be.visible')
 
     // scroll the button into view, as if the user had scrolled
-    cy.get('#scroll-horizontal button').scrollIntoView()
-      .should('be.visible')
+    cy.get('#scroll-horizontal button').scrollIntoView().should('be.visible')
 
-    cy.get('#scroll-vertical button')
-      .should('not.be.visible')
+    cy.get('#scroll-vertical button').should('not.be.visible')
 
     // Cypress handles the scroll direction needed
-    cy.get('#scroll-vertical button').scrollIntoView()
-      .should('be.visible')
+    cy.get('#scroll-vertical button').scrollIntoView().should('be.visible')
 
-    cy.get('#scroll-both button')
-      .should('not.be.visible')
+    cy.get('#scroll-both button').should('not.be.visible')
 
     // Cypress knows to scroll to the right and down
-    cy.get('#scroll-both button').scrollIntoView()
-      .should('be.visible')
+    cy.get('#scroll-both button').scrollIntoView().should('be.visible')
   })
 
   it('.trigger() - trigger an event on a DOM element', () => {
@@ -256,7 +287,8 @@ context('Actions', () => {
     cy.get('.trigger-input-range')
       .invoke('val', 25)
       .trigger('change')
-      .get('input[type=range]').siblings('p')
+      .get('input[type=range]')
+      .siblings('p')
       .should('have.text', '25')
   })
 
